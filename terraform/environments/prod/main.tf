bce1e70f95122db475e3a3dc728d7d98bd2693ed
rizz-cloud <rh05@hotmail.co.uk>
2026-09-18 17:58:10 +0100

    harden ECS deployment configuration

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -77,6 +77,10 @@ module "ecs" {
   execution_role_arn = module.iam.ecs_task_execution_role_arn
   task_role_arn      = module.iam.ecs_task_role_arn
 
+  # Ensures the ALB listener exists before ECS registers the service.
+  depends_on = [module.alb]
+
   tags = local.common_tags
+
 }
 
