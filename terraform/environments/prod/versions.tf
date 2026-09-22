a75653d031ce5a7a30d4f7089358b0e2078a86bd
rizz-cloud <rh05@hotmail.co.uk>
2026-09-22 17:19:34 +0100

    add Terraform CI validation checks

--- terraform/environments/prod/versions.tf
+++ terraform/environments/prod/versions.tf
@@ -9,11 +9,11 @@ terraform {
   }
 
   # Stores production Terraform state remotely in S3 with native state locking.
-backend "s3" {
-  key          = "prod/terraform.tfstate"
-  region       = "eu-west-2"
-  encrypt      = true
-  use_lockfile = true
-}
+  backend "s3" {
+    key          = "prod/terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
 
