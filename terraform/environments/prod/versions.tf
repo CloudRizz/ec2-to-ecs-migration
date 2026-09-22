710357a39458c57261acb9c03c054b9eca2a2f67
rizz-cloud <rh05@hotmail.co.uk>
2026-09-22 17:07:46 +0100

    configure S3 remote state for production

--- terraform/environments/prod/versions.tf
+++ terraform/environments/prod/versions.tf
@@ -7,4 +7,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  # Stores production Terraform state remotely in S3 with native state locking.
+backend "s3" {
+  key          = "prod/terraform.tfstate"
+  region       = "eu-west-2"
+  encrypt      = true
+  use_lockfile = true
 }
+}
+
